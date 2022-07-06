4a76566f24809755b40b37da75ec0f209cca5154
MahekChudesara <mahekchudesara12@gmail.com>
2022-07-06 10:59:58 +0530

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,41 @@
+const Sdata=
+[
+{
+    
+    aname:"Naruto",
+    title:" Netflix Anime",
+    imgsrc:"https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABemh0OSq9mGU8Fu7ZTHpyVrUpiS_EqH-wDaUnmJpI3H_MGZ5MxT-cgmJAUJ_TLDUUR7WoVm8RK7SabcaXs4wRQBtGmucAwWvR9U.jpg?r=58b",
+    alink:"https://www.netflix.com/in/title/70205012"
+},
+{
+    aname:"Dr.stone",
+    title:" Netflix Anime",
+    imgsrc:"https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABWWNLY5L7vlLx2V6I2jWvsuKIWLSX6BqQK8bDnMqy73O4gWNY5YdyExZ45cnkDN3tXCAsHx80sG6Q1kZtOnIvJf1rxOuHJfNuZk.jpg?r=ba0",
+    alink:"https://www.netflix.com/in/title/81046193"
+},
+{
+    aname:"Death note",
+    title:" Netflix Anime",
+    imgsrc:"https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABYMoP5OBvpCnXeETMVwPkcbZaZFFVevE2TiUKEWGpKHytjqlzI7AUG_5I8dcukiNNfRATjyuND2c4apsdE6J3dd-z9J4e4vq0tk.jpg?r=f47",
+    alink:"https://www.netflix.com/in/title/70204970"
+},
+{
+    aname:"Jujutsu Kaisen",
+    title:" Netflix Anime",
+    imgsrc:"https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTrRwpL45cu0MOHvh_N8BK1wLAyLYyjqkvwJRn4qgwJpEKKDxMzE7JZO9NOPdBSePTjRDF-MU6FueqiP66G0Q5CcGOq6XQeSz5U.jpg?r=967",
+    alink:"https://www.netflix.com/in/title/81278456"
+},
+{
+    aname:"Demon slayer",
+    title:" Netflix Anime",
+    imgsrc:"https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeyUI2oIHS3MyVyQU2929HjhrZRUuZiOvvsFdm3DYsRTwwt83LiI1Z2ciALrCFluemzQECozWrVYbtJs0LOKX8SXKa2xfr0YKv4.jpg?r=043",
+    alink:"https://www.netflix.com/in/title/81091393"
+},
+{
+    aname:"Black clover",
+    title:" Netflix Anime",
+    imgsrc:"https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcJ9Nwkgb7QczI8pekIqeI-09ZVWtNk9biGtSkuUBp6KvU_W7s16XuLnjbjBnyWjEIsEz6zz9dSovMb11opUhBQTT4SPr3acsqA.jpg?r=9ce",
+    alink:"https://www.netflix.com/in/title/80238012"
+}
+];
+export default Sdata;
